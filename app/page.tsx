b9cedb6392d56b73e195c796f15366c149ac4dc6
Richardpaz <richardrafaelpaz@gmail.com>
2026-05-08 11:04:14 -0300

    sobre mi

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,11 @@
+import { Text } from "@radix-ui/themes"
 function Home() {
-    return(
-        <h1>home</h1>
+    return (
+        <Text>
+            Sobre mí
+            Soy Analista de Sistemas con orientación al desarrollo de aplicaciones web modernas, enfocado en crear soluciones eficientes, escalables y centradas en el usuario.
+            Cuento con experiencia en desarrollo Full Stack, análisis funcional y diseño de sistemas, integrando frontend, backend y bases de datos en aplicaciones reales.
+        </Text>
     )
 }
 export default Home;
