5fa18d492e2a4e1cee9724c8935bdd0294aa52ba
Richardpaz <richardrafaelpaz@gmail.com>
2026-05-08 15:33:08 -0300

    edit de pestaña contacto

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,17 +1,23 @@
-import React from "react";
 import { Resend } from "resend";
 import { NextResponse } from "next/server";
-import { EmailTemplate } from "../components/EmailTemplate";
+import type { NextRequest } from "next/server";
 
 const resend = new Resend("re_Xpp51Fxx_4h7pp67vPNkucsSL6xHDcjVo");
 
-export async function POST() {
+export async function POST(req: NextRequest) {
+    const { nombre, email, mensaje } = await req.json();
+
+    const span = `<div>
+                    <p>nombre : ${nombre}</p>
+                    <p>email : ${email}</p>
+                    <p>mensaje : ${mensaje}</p>
+                </div>`
     try {
         const data = await resend.emails.send({
-            from: "Portfolio <onboarding@resend.dev>",
-            to: ["richardrafaelpaz@gmail.com"],
-            subject: "Nuevo mensaje",
-            react: `<span>Email enviado</span>`,
+            from: 'Portafolio <onboarding@resend.dev>',
+            to: ['richardrafaelpaz@gmail.com'],
+            subject: 'portafolio/empleo',
+            html: span,
         });
 
         return NextResponse.json(data);
