b4c2fa83e1620e92e149bb3e5b658e7bd2cb47c3
Richardpaz <richardrafaelpaz@gmail.com>
2026-05-08 14:52:07 -0300

    proyecto y error tipado badge

--- app/Proyectos/page.tsx
+++ app/Proyectos/page.tsx
@@ -1,21 +1,57 @@
 import { Badge, Card, Flex, Heading, Text } from "@radix-ui/themes";
 import Image from "next/image";
 import Link from "next/link";
+import { string, z } from "zod"
 
-const tecnologia = [
-    { name: "nodejs", color: "green" },
-    { name: "nextjs", color: "black" },
-    { name: "tailwindcss", color: "blue" },
-    { name: "supabase", color: "green" },
-    { name: "zod", color: "red" },
-    { name: "typescript", color: "blue" },
-    { name: "zustand", color: "brown" }
-]
+const badgeColors = [
+    "green",
+    "brown",
+    "blue",
+    "red",
+    "ruby",
+    "gray",
+    "gold",
+    "bronze",
+    "yellow",
+    "amber",
+    "orange",
+    "tomato",
+    "crimson",
+    "pink",
+    "plum",
+    "purple",
+    "violet",
+    "iris",
+    "indigo",
+    "cyan",
+    "teal",
+    "jade",
+    "grass",
+    "lime",
+    "mint",
+    "sky",
+] as const;
 
+const techSchema = z.object({
+    name: z.string(),
+    color: z.enum(badgeColors)
+})
+
+type typeTech = z.infer<typeof techSchema>
+
+const tecnologia: typeTech[] = [
+    { name: "nodejs", color: 'green' },
+    { name: "nextjs", color: 'brown' },
+    { name: "tailwindcss", color: 'blue' },
+    { name: "supabase", color: 'green' },
+    { name: "zod", color: 'red' },
+    { name: "typescript", color: 'blue' },
+    { name: "zustand", color: 'brown' }
+] 
 
 function Proyectos() {
     return (
-        <Flex justify={"center"} direction={"column"} gap={"2"} style={{ padding: "0 50px"}}>
+        <Flex justify={"center"} direction={"column"} gap={"2"} style={{ padding: "0 50px" }}>
             <Card variant="classic" className="border-1 border-gray-400 h-50">
                 <Flex direction={"column"} gap={"4"}>
                     <Text weight={"medium"}>Gestión de Tareas</Text>
@@ -24,7 +60,7 @@ function Proyectos() {
                     </Text>
 
                     <Flex gap={"2"}>
-                        {tecnologia.map(t => <Badge variant={"outline"} color={t.color} key={t.name}>{t.name}</Badge>)}
+                        {tecnologia?.map((t) => <Badge variant={"outline"} color={t.color} key={t.name}>{t.name}</Badge>)}
                     </Flex>
                     <Link href={"https://richardpaz-todo-app.vercel.app/"} target={"_blank"}>
                         <Image
